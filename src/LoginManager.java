5adef2260b9c58750ca4cc3463b8f7495b076138
charanroz <rozcharan@gmail.com>
2026-03-07 03:56:15 +0000

    Updated login class for staff

--- src/LoginManager.java
+++ src/LoginManager.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.util.List;
 
 public class LoginManager {
+    public Staff login(String email, String password, List<Staff> staffList){
+        for(Staff staff : staffList){
+            if(staff.getEmail().equals(email) && staff.getPassword().equals(password)){
+                return staff;
+            }
+        }
+        return null;
+    }
 }
--- src/Staff.java
+++ src/Staff.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Staff {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+
+    public Staff(int id, String name, String email, String password){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
