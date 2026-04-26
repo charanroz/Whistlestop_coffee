a136a985377e536b61eb60c2f6007fdc5617701f
c5031171Newcastle <c5031171@newcastle.ac.uk>
2026-04-26 19:42:21 +0100

    Convert repositories to JpaRepository

--- backend/src/main/java/com/whistlestop_coffee/whistlestop_coffee/repository/CustomerRepository.java
+++ backend/src/main/java/com/whistlestop_coffee/whistlestop_coffee/repository/CustomerRepository.java
@@ -1,31 +1,11 @@
 package com.whistlestop_coffee.whistlestop_coffee.repository;
 
 import com.whistlestop_coffee.whistlestop_coffee.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class CustomerRepository {
-
-    private List<Customer> customers;
-
-    public CustomerRepository() {
-        customers = new ArrayList<>();
-
-        // 模拟数据库里的用户
-        customers.add(new Customer("Alice", "alice@email.com", "123456"));
-        customers.add(new Customer("Bob", "bob@email.com", "password"));
-    }
-
-    public Customer findByEmail(String email) {
-
-        for (Customer c : customers) {
-            if (c.getEmail().equals(email)) {
-                return c;
-            }
-        }
-
-        return null;
-    }
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    Optional<Customer> findByEmail(String email);
 }
-
