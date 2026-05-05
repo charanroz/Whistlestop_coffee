3c89cbd5ad705c80e6b1ba7672a2b8a4f09e7481
charanroz <rozcharan@gmail.com>
2026-05-05 01:51:27 +0100

    fix cors for vercel

--- backend/src/main/java/com/whistlestop_coffee/whistlestop_coffee/config/CorsConfig.java
+++ backend/src/main/java/com/whistlestop_coffee/whistlestop_coffee/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://whistlestop-coffee-je2g.vercel.app"
+                        )
                         .allowedMethods("*");
             }
         };
